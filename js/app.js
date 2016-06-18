1f9e5bce575a0e43e0ecd0d7cee2a65e3c5f7faf
Matthew Park <pjh1821@gmail.com>
2016-06-18 16:53:14 +0900

    버그 수정

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-function generate() {  var numbers = [];  for(var i=0; i<6; i++) {    numbers.push(Math.floor(Math.random() * 45));  }  return numbers; } 
+function generate() {  var numbers = [];  for(var i=0; i<6; i++) {    numbers.push(Math.floor(Math.random() * 45)+1);  }  return numbers; } 
