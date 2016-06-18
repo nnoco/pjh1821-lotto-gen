19024ab1ed27a1566fd8c8a2269c630b4c094c75
junyoung <junyoung.plum@gmail.com>
2016-06-18 17:36:43 +0900

    정렬기능 추가

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
 on generate() {  var numbers = [];  for(var i=0; i<6; i++) {    numbers.push(Math.floor(Math.random() * 45)+1);  }  return numbers; } 
 ion generate() {  var numbers = [];  for(var i=0; i<6; i++) {    numbers.push(Math.floor(Math.random() * 45)+1);  }  return numbers; } //=
+
+// 정렬 기능 추가
